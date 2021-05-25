e7d41e9432f9c09a32d445e2a11d050b8c7cc967
Azhaar Fithri Shidqiyah <0320200005@polman.astra.ac.id>
2021-05-25 19:04:08 +0700

    MyLogin

--- MyLogin/Form1.cs
+++ MyLogin/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
 
 namespace MyLogin
 {
@@ -15,6 +16,14 @@ namespace MyLogin
         public Form1()
         {
             InitializeComponent();
+
+            //ambil dari app setting dan masukkan ke variabel
+            String valueTestString = ConfigurationManager.AppSettings["TestString"];
+            int valueTestAngka = int.Parse(ConfigurationManager.AppSettings["TestAngka"]);
+
+            //menampilkan variabel via message box
+            MessageBox.Show("Isi Test String: " + valueTestString);
+            MessageBox.Show("Isi Test Angka: " + valueTestAngka);
         }
         DataClasses1DataContext db = new DataClasses1DataContext();
         private void btnLogin_Click(object sender, EventArgs e)
